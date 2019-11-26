4c2dc392cc56be97d9c15a501713e6b73c1442d2
Macheza Dzabala <dzabalamacheza@gmail.com>
2019-11-26 13:30:06 +0200

    CH Setup Redux
    - Set up redux in the application
    
    [finishes #168781613]

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
 
-const App = () => (
-    <div>
-        <h1> Welcome to Barefoot Nomad App</h1>
-    </div>
-);
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fireReduxAction } from '../actions/defaultAction';
 
-export default App;
+class App extends Component {
+  componentDidMount() {
+    const { props } = this;
+    props.fireReduxAction();
+  }
+
+  render() {
+    const { props: { default: { text } } } = this;
+    return (
+      <div>
+        <h1>Welcome to barefoot Nomad</h1>
+        <h3>{text}</h3>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  fireReduxAction: PropTypes.func.isRequired,
+  default: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  default: state.default,
+});
+
+export default connect(mapStateToProps, { fireReduxAction })(App);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './reduxStore';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
+const Root = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('app'));
