0c477cd64dd75429f0005d8ead42546256d57eaf
Alain <alainmateso@gmail.com>
2019-11-26 16:13:25 +0200

    Chore(Routing): setup react router - sets up react router - sets up /users dummy route - arranges views and components separately [Finishes#168781616]

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const App = () => (
     <div>
         <h1> Welcome to Barefoot Nomad App</h1>
+        <h2>
+<Link to="/users">Go to users route</Link>
+        </h2>
     </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './components/App';
+import Users from './views/Autthentication';
+import NotFound from './components/NotFound';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const Routes = (
+    <Router>
+        <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/users" component={Users} />
+        <Route path="*" component={NotFound} />
+        </Switch>
+    </Router>
+);
+
+ReactDOM.render(Routes, document.getElementById('app'));
