e3ca23505051fa376ff47efcd7374e6870b008f9
Miriam Nakiwuge <nakiwuge@gmail.com>
2019-12-12 21:17:13 +0300

    Revert "#168781646 create accommodation"

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import verify from './verifyUserReducer';
 import resetpasswordreducer from './resetPasswordReducer';
-import accommodationReducer from './AccommodationReducer';
 
 export default combineReducers({
   auth: authReducer,
   verify,
   response: resetpasswordreducer,
-  accommodation: accommodationReducer,
 });
