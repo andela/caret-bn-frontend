9e69e5d4a464cb1f39195247c7f027f997a095ad
sabin18 <sabin6341@gmail.com>
2019-12-16 21:58:42 +0200

    bug(deploy on review app):configure review app add app.json [Finishes#170319641]

--- src/views/accommodations/AllAccommodations.js
+++ src/views/accommodations/AllAccommodations.js
@@ -1,27 +1,13 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import { Button, ButtonToolbar } from 'react-bootstrap';
-import { Add } from '@material-ui/icons';
-import Row from 'react-bootstrap/Row';
-import { checkSupplier } from '../../helpers/authHelper';
 import AllAccommodation from '../../components/pages/AllAccommodation';
 
 export class GetAllAccommodations extends Component {
   render() {
     return (
-            <div>
-                { checkSupplier() ? (
-                    <ButtonToolbar>
-                         <Row className="createButton">
-                          <Button href="/accommodations/new">
-                            <Add />
-                            Create new accommodation
-                          </Button>
-                         </Row>
-                        <AllAccommodation data-test="AllAccommodation" />
-                    </ButtonToolbar>
-                ) : null}
-            </div>
+             <div>
+                <AllAccommodation data-test="AllAccommodation" />
+             </div>
     );
   }
 }
